8754f29f6f81b781ef68f403be85e4b7ffbdf8c4
SYAM K S <65106995+SKS22@users.noreply.github.com>
2020-11-29 18:49:08 +0530

    Update main.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,10 +65,7 @@ USER_SETTINGS = {}
 GDPR = []
 
 START_IMG = os.environ.get('START_IMG', None)
-if START_IMG is None:
-    img = "https://telegra.ph/file/fc734b227985a1524e715.jpg"
-else:
-  img = START_IMG    
+
     
 for module_name in ALL_MODULES:
     imported_module = importlib.import_module("bot.modules." + module_name)
